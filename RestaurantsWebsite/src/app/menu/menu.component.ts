fe28431ab103c61f28c23fc2a11bf1bfa16105ec
Ajmal T A <ajmaltaadivaram@gmail.com>
2023-07-29 20:28:35 +0530

    MenuCreation Complete

--- RestaurantsWebsite/src/app/menu/menu.component.ts
+++ RestaurantsWebsite/src/app/menu/menu.component.ts
@@ -7,6 +7,18 @@ import { Component,OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit{
 
+
+  Fooditems=[{img:'../../assets/beefandporatta.jpg',name:'Poratta',price:13},
+  {img:'../../assets/biriyani.jpg',name:'Briyani',price:130},
+  {img:'../../assets/dhosha.jpg',name:'Dhosha',
+  price:13},{img:'../../assets/chikencurry.jpg',name:'Chikken Cuury',price:13},
+  {img:'../../assets/kayyappam.jpg',name:'Kayyapam',price:13},
+  {img:'../../assets/pups.jpg',name:'Pups',price:13},
+  {img:'../../assets/samoosa.jpg',name:'Samoosa',price:13},
+  {img:'../../assets/meals.jpg',name:'Meals',price:13},
+  {img:'../../assets/fishfry.jpg',name:'Fish Fry',price:13},
+  {img:'../../assets/beeffry.jpg',name:'Beef Fry',price:13}]
+
 constructor(){}
 ngOnInit(): void {
   
